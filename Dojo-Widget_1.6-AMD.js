b05bccb7d6f9b9d01fab4c9c4c72aa286d3bbdef
Benjamin Sternthal <ben@devpatch.com>
2011-07-06 11:09:41 -0700

    Stubbed out Dojo Widget 1.6, need more input on this one.

---
+++ Dojo-Widget_1.6-AMD.js
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+
+
+define("ui/widgetName", ["dojo", "dijit/_Widget", "dijit/_Templated"], function(dojo, Widget, Templated) {
+
+    /* not sure best format for code in here */
+
+});
+
